a2af3e707aeb7ed856647843450bfaa721573c92
Wu Luo <lwyeluo@pku.edu.cn>
2017-09-06 21:07:40 +0800

    Remove unuseful files.

--- test/hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/usr/src/Trusted-Container/test/hello/hello.ko := ld -r -m elf_x86_64 -T /usr/src/Trusted-Container/linux-source-3.13.0/scripts/module-common.lds --build-id  -o /usr/src/Trusted-Container/test/hello/hello.ko /usr/src/Trusted-Container/test/hello/hello.o /usr/src/Trusted-Container/test/hello/hello.mod.o
